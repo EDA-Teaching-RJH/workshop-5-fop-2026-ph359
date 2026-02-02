c5bd2d6611f7668d60a70e45025a128720c66c7a
Philip <ph359@kent.ac.uk>
2026-02-02 13:46:38 +0000

    1.1

--- camel.py
+++ camel.py
@@ -1,1 +1,13 @@
 # Enter your code here
+camel_name = input("Enter your name in Camel Case: ")
+snake_name = ""
+
+for char in camel_name:
+    if char.isupper():
+        snake_name += "_" + char.lower()
+    else:
+        snake_name += char
+
+print("Name in Snake: " , snake_name)
+
+
