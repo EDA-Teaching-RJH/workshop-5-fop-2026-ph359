78ea7fb0ccda558735b50ce98d4186a1ac205b60
Philip <ph359@kent.ac.uk>
2026-02-02 14:33:17 +0000

    1.1

--- coffee.py
+++ coffee.py
@@ -1,1 +1,17 @@
-#Your code goes here
+price = 75
+accepted_coin = [50,20,10,5]
+
+while price > 0:
+    print(f"Price: {price}p")
+
+    coin = int(input("Enter coins (accepted coins: 50p, 20p, 10p, 5p)"))
+
+    if coin in accepted_coin:
+        price -= coin
+    else:
+        print("Enter a valid coin")
+
+print("Thank You! Dispensing Coffee")
+change = abs(price)
+
+print(f"Change: {change}p")
